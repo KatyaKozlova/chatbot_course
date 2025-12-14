88e42ce26cd568e9b6b47de8051b094f7afbb2d2
KatyaKozlova <katyakozlova000mail.ru>
2025-12-14 19:36:41 +0300

    changed main

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,24 +12,29 @@ dotenv.load_dotenv()
 
 dispatcher = Dispatcher()
 
+
 @dispatcher.update.outer_middleware()
 async def logger_middleware(handler: callable, event: Update, data: dict):
-    payload=deserialize_telegram_object_to_python(event)
+    payload = deserialize_telegram_object_to_python(event)
     print(json.dumps(payload, ensure_ascii=False, indent=2))
-    return await handler(event,data)
+    return await handler(event, data)
+
 
 @dispatcher.message(F.text)
 async def message_text_echo_handler(message: Message):
     await message.answer(message.text)
 
+
 @dispatcher.message(F.photo)
 async def message_photo_echo_handler(message: Message):
     await message.answer_photo(message.photo[-1].file_id)
+
 
 async def main() -> None:
     bot = Bot(token=os.getenv("TELEGRAM_TOKEN"))
     await dispatcher.start_polling(bot)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
